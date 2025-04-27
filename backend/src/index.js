921c67ba9a48f9a3e644bb3ca4b9c9dce4eb33b4
Shann Lacanaria <shannrhey123@gmail.com>
2025-04-28 01:09:04 +0800

    Task: reviewing

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,11 +2,13 @@ import express from "express";
 import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);   //for authentication (login, signup, logout);
--- frontend/src/components/signup.jsx
+++ frontend/src/components/signup.jsx
@@ -1,48 +1,90 @@
 import { FaBeer, FaQuestion } from 'react-icons/fa';
 
+import axios from "axios"
+import { useState } from 'react';
+
 export default function Signup(){
 
+    const [signUpForm, setSignUpForm] = useState({fullName : "", email : "", password : ""});
 
 
-    async function onSubmit(){
+    async function handleOnSubmit(event){
+        event.preventDefault();
+
         try {
+
+
+            // if (!signUpForm.fullName || !signUpForm.email || !signUpForm.password){
+            //     console.log("Fill out all ");
+            // }
+
+            const res = await axios.post(`http://localhost:5001/api/auth/signup`, signUpForm);
+            console.log("Sign in succesfully!");
+
+            setSignUpForm({fullName : "", email : "", password : ""});
             
-            
+
 
         } catch (e) {
-            
+            console.log("Error in submit: ", e.message);
         }
+    }
+
+    async function handleLogOut(){
+        try{
+            const res = await axios.post("http://localhost:5001/api/auth/logout");
+            console.log("Logout successfully!");
+        }catch(e){
+            console.log("Error in logout : ", e.message);
+        }
+    }
+
+    function handleOnChange(e){
+        setSignUpForm((prev) => ({
+            ...prev,
+            [e.target.name] : e.target.value
+        }))
     }
 
 
     return <div className="w-full h-screen flex flex-col items-center bg-[#F1E7E7] ">
         <h1 className="w-[90%] text-2xl lg:text-5xl mt-6 underline underline-offset-4 ">CHAT</h1>
 
-        <div className="w-[30%] border-1 min-h-[400px] h-[450px] flex-col bg-[#E69DB8] mt-5 rounded-[20px] shadow-lg
+        <form onSubmit={handleOnSubmit} className="w-[30%] border-1 min-h-[400px] h-[450px] flex-col bg-[#E69DB8] mt-5 rounded-[20px] shadow-lg
         items-center flex ">
             <h1 className="w-[100%] text-center mt-4 text-3xl ">SIGN UP</h1>
 
             <div className="w-[70%] mt-5 flex flex-col">
+
                 <label htmlFor="" className="mb-3">Full Name</label>
-                <input type="text" className="p-4 border-1 h-[50px] bg-white" placeholder="Full Name"/>
+                <input type="text" className="p-4 border-1 h-[50px] bg-white" placeholder="Full Name" 
+                name='fullName' value={signUpForm.fullName} onChange={handleOnChange} autoComplete='off' required/>
+
             </div>
             <div className="w-[70%] mt-2  flex flex-col">
+
                 <label htmlFor="" className="mb-3">Email</label>
-                <input type="text" className="p-4 border-1 h-[50px] bg-white" placeholder="Email"/>
+                <input type="text" className="p-4 border-1 h-[50px] bg-white" placeholder="Email"
+                name='email' value={signUpForm.email} onChange={handleOnChange} autoComplete='off' required/>
+
             </div>
             <div className="w-[70%] mt-2 flex flex-col mb-8">
+
                 <label htmlFor="" className="mb-3">Password</label>
-                <input type="text" className="p-4 border-1 h-[50px]  bg-white" placeholder="Password"/>
+                <input type="password" className="p-4 border-1 h-[50px]  bg-white" placeholder="Password"
+                name='password' value={signUpForm.password} onChange={handleOnChange} autoComplete='off' required/>
+
             </div>
+
 
             <button className="border-1 w-[150px] h-[40px] rounded-[500px] cursor-pointer bg-white
             hover:bg-gray-200 active:scale-95">SIGN UP</button>
 
-        </div>
+        </form>
 
         <div className='w-[90%] h-[60px] flex justify-end items-center'>
             <div className=' w-[60px] h-[100%] flex justify-center items-center scale-115 rounded-[500px] bg-[#FFFECE]'>
-                <h1 className='text-2xl lg:text-3xl text-black cursor-pointer'><FaQuestion/></h1>
+                <h1 className='text-2xl lg:text-3xl text-black cursor-pointer active:scale-95' onClick={handleLogOut}><FaQuestion/></h1>
             </div>
             
         </div>
