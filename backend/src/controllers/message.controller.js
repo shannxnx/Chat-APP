fd2bd653cf5d9bfb1a0aaec20a258da9ae143657
Shann Lacanaria <shannrhey123@gmail.com>
2025-05-02 01:24:59 +0800

    clean up

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -7,8 +7,8 @@ export const getUsersForSidebar = async (req, res) => {
 
         //this returns all the users except the logged in user and the password
         //$ne = not equal
+        
         const filteredUsers = await User.find({_id: {$ne:loggedInUserId}}).select("-password");
-
         res.status(200).json(filteredUsers);
     } catch (error) {
         console.log("Error in getting users (sidebar): ", error.message);
