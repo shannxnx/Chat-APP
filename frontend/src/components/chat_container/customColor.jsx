532ccd3eae20dbe5ebc11be2ae1e073a35bc1fe9
Shann Lacanaria <shannrhey123@gmail.com>
2025-05-13 02:03:06 +0800

    fuck hell shit

--- frontend/src/components/chat_container/customColor.jsx
+++ frontend/src/components/chat_container/customColor.jsx
@@ -1,32 +1,59 @@
 import { useEffect } from "react";
 import { useAuthStore } from "../../store/useAuthStore";
 import { useChatStore } from "../../store/useChatStore"
+import axios from "axios";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
 export default function ColorDiv({color}){
 
-    const {setBgColor, chatBgColor} = useChatStore();
-    const {selectedChat, authUser} = useAuthStore();
+    const {setBgColor, chatBgColor, selectedChat} = useChatStore();
+    const {authUser} = useAuthStore();
+
+    const [bgData, setBgData] = useState({});
 
     const handleCustomColor = (color) => {
         setBgColor(color)
     }
 
-    async function ChangeBgColor(params) {
-        
+    async function ChangeBgColor(data) {
+        try {
+            if (selectedChat._id){
+                const res = await axios.post(`http://localhost:5001/api/chatBg/change-ChatBg/${selectedChat._id}`, data, {withCredentials : true});
+                setBgData(res.data);
+            }
+           
+        } catch (error) {
+            console.log("Error in change BG color : ", error.message);
+            toast.error(error?.response?.data?.message);
+            
+        }
     }
+
+    
 
     async function handleCustomChat(e) {
         e.preventDefault;
+        const data = {chatBackground : color};
+        ChangeBgColor(data);
         
     }
 
+    console.log("bgData", bgData);
+
+    
+
+    
+    
     
 
     
 
-   return <form className={`size-[64px] transition-transform
+    
+
+   return <div className={`size-[64px] transition-transform
    rounded cursor-pointer hover:scale-115`} style={{backgroundColor : `${color}`, opacity : 1  }}
-   onSubmit={handleCustomChat}>
+   onClick={handleCustomChat}>
 
-    </form>
+    </div>
 }
